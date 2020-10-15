cb8867aaf56daeace00e870226c621e0738fe3ea
root <root@DESKTOP-LDB79B4.localdomain>
2020-10-15 17:44:59 +0200

    argv parsed

--- minishell/srcs/loop.c
+++ minishell/srcs/loop.c
@@ -13,10 +13,10 @@ void			launching_loop(t_data *data)
 
         //print cmd
         t_command *c = current->content;
-		ft_putstr_fd("--- str ---\n", 1);
+		ft_putstr_fd("--- str --- \n", 1);
 	    ft_putstr_fd(c->str, 1);
 	    ft_putstr_fd("\n\n", 1);
-		ft_putstr_fd("--- argv ---\n", 1);
+		ft_putstr_fd("--- argv --- \n", 1);
 		for (int i = 0; c->argv[i]; i++)
 		{
 			ft_putstr_fd(c->argv[i], 1);
